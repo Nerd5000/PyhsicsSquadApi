166b1568dcc5308f0d2ae30270a1200000a4530e
Muhammed AbdElrhman <ma.prog.zero@gmail.com>
2020-03-01 00:26:04 +0200

    updating , adding documentation

--- sheets/delete.php
+++ sheets/delete.php
@@ -3,15 +3,16 @@ include 'sheets.php';
 header("Access-Control-Allow-Origin: *");
 
 
-if (!isset($_GET['all']) || !isset($_GET['first']) || !isset($_GET['first'])) {
+if (isset($_GET['all']) || isset($_GET['first']) || isset($_GET['first'])) {
+
+    if ($_GET['all'] == 'true') {
+        deleteAllSheets();
+    } elseif ($_GET['first'] == 'true') {
+        deleteFirstSheet();
+    } elseif ($_GET['last'] == 'true') {
+        deleteLastSheet();
+    }
+    echo 'done';
+} else {
     exit;
 }
-
-if ($_GET['all'] == 'true') {
-    deleteAllSheets();
-} elseif ($_GET['first'] == 'true') {
-    deleteFirstSheet();
-} elseif ($_GET['last'] == 'true') {
-    deleteLastSheet();
-}
-echo 'done';
